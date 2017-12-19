020950b286efdf09f1ac4e1eb016a755c50b455d
Peter Burns <peterburnshicks@gmail.com>
2017-12-18 22:16:57 -0500

    finished BST

--- BinaryStorageTree.js
+++ BinaryStorageTree.js
@@ -15,20 +15,57 @@ BST.prototype.insert = function (value) {
     }
 }
 BST.prototype.contains = function (value) {
-    if(value === this.value)return true;
-    else if(value<this.value){
-        if(!this.left)return false;
+    if (value === this.value) return true;
+    else if (value < this.value) {
+        if (!this.left) return false;
         else return this.left.contains(value);
     }
-    else if(value>this.value){
-        if(!this.right)return false;
+    else if (value > this.value) {
+        if (!this.right) return false;
         else return this.right.contains(value);
     }
 };
+BST.prototype.depthFirstTraversal = function (iteratorFunc, order) {
+    if (order == 'pre-order') iteratorFunc(this.value);
+    if (this.left) this.left.depthFirstTraversal(iteratorFunc, order);
+    if (order == 'in-order') iteratorFunc(this.value);
+    if (this.right) this.right.depthFirstTraversal(iteratorFunc, order);
+    if (order == 'post-order') iteratorFunc(this.value);
+};
+BST.prototype.breadthFirstTraversal = function (iteratorFunc) {
+    var queue = [this];
+    while (queue.length) {
+        var treeNode = queue.shift();
+        iteratorFunc(treeNode);
+        if (treeNode.left) queue.push(treeNode.left);
+        if (treeNode.right) queue.push(treeNode.right);
+    }
+};
+BST.prototype.getMinVal = function () {
+    if (this.left) return this.left.getMinVal();
+    else return this.value;
+};
+BST.prototype.getMaxVal = function () {
+    if (this.right) return this.right.getMaxVal();
+    else return this.value;
+};
+
 var bst = new BST(50);
+bst.insert(30);
 bst.insert(45);
+bst.insert(20);
+bst.insert(10);
 bst.insert(35);
-bst.insert(25);
-bst.insert(115);
-bst.insert(50);
-console.log(bst.contains(25));
+bst.insert(70);
+bst.insert(60);
+bst.insert(59);
+bst.insert(100);
+bst.insert(85);
+bst.insert(105);
+function log(value) {
+    console.log(value.value);
+}
+var min = bst.getMinVal();
+var max = bst.getMaxVal();
+console.log(min);
+console.log(max);
