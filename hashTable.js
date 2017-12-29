18d8d3a15016146dbd73b1f9ac4a7c567545006a
Peter Burns <peterburnshicks@gmail.com>
2017-12-28 23:34:39 -0500

    added insert function

--- hashTable.js
+++ hashTable.js
@@ -10,9 +10,23 @@ function HashNode(key, value, next) {
 }
 
 HashTable.prototype.hash = function (key) {
-    return key.split('').reduce(function(a,b){return a + b.charCodeAt(0);},0)%this.numBuckets;
+    return key.split('').reduce(function (a, b) { return a + b.charCodeAt(0); }, 0) % this.numBuckets;
+};
+
+HashTable.prototype.insert = function (key, value) {
+    var index = this.hash(key);
+    if (!this.buckets[index]) this.buckets[index] = new HashNode(key, value);
+    else {
+        var currentNode = this.buckets[index];
+        while (currentNode.next) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = new HashNode(key, value);
+    }
 };
 
 var myHT = new HashTable(30);
-console.log(myHT.hash('Becca'));
-//console.log(myHT);
+myHT.insert('Dean','dean@gmail.com');
+myHT.insert('Megan','megan@gmail.com');
+myHT.insert('Dane','dane@yahoo.com');
+console.log(myHT.buckets);
