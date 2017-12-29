5bbfb0234d540ced97c8db2de1a69c90d1a424a6
Peter Burns <peterburnshicks@gmail.com>
2017-12-28 23:05:15 -0500

    added hash function

--- hashTable.js
+++ hashTable.js
@@ -1,13 +1,18 @@
-function HashTable (size) {
+function HashTable(size) {
     this.buckets = Array(size);
     this.numBuckets = this.buckets.length;
 }
 
-function HashNode (key, value, next) {
+function HashNode(key, value, next) {
     this.key = key;
     this.value = value;
     this.next = next || null;
 }
 
-var myHT = new HashTable (30);
-console.log(myHT);
+HashTable.prototype.hash = function (key) {
+    return key.split('').reduce(function(a,b){return a + b.charCodeAt(0);},0)%this.numBuckets;
+};
+
+var myHT = new HashTable(30);
+console.log(myHT.hash('Becca'));
+//console.log(myHT);
